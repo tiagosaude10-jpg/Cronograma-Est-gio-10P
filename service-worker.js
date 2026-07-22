39d946895ca62d736321cddac67e8ac61746caf0
tiagosaude10-jpg <tiagosaude10@gmail.com>
2026-07-22 10:10:30 -0400

    Atualizar cache para pesquisa por preceptora

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,10 @@
-const CACHE_NAME = 'cronogramas-internato-v13';
+const CACHE_NAME = 'cronogramas-internato-v14';
 const APP_SHELL = [
   './',
   './index.html',
   './today-v11.js',
   './search-student-v2.js',
+  './search-preceptor-v1.js',
   './manifest-estagio-10p.webmanifest',
   './icons/icon-192-v3.png',
   './icons/icon-512-v3.png',
